6ebbb25c4600a69a4a50efc6d77851edd778e373
Fareez Borhanudin <fare4z@gmail.com>
2019-10-03 16:57:13 +0800

    Update home_model.php
    propose new model

--- application/models/home_model.php
+++ application/models/home_model.php
@@ -1,1 +1,26 @@
-<?phpclass Home_model extends MY_Model {	protected $_table = 'tb_news';	protected $primary_key = 'news_id';	function sort_date()	{		$this->db->order_by('news_date','desc');		return $this;	}}?>
+<?php
+class Home_model extends MY_Model {
+
+	protected $_table = 'tb_news';
+	protected $primary_key = 'news_id';
+
+	function sort_date()
+	{
+		$this->db->order_by('news_date','desc');
+		return $this;
+	}
+  
+  
+  function get_info_multiple_cond($table, $where = 0) {
+		$this->db->select('*');
+		$this->db->from($table);
+		if ($where != 0) {
+			foreach ($where as $key => $val) {
+				$this->db->where($key, $val);
+			}
+		}
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+}
+?>
